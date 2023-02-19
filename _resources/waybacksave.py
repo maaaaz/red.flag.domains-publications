6f0e952ac269be9933c2d95ccc16acf9c2acdbd0
maaaaz <maaaaz@users.noreply.github.com>
2023-02-19 10:26:30 +0100

    adding wayback save

---
+++ _resources/waybacksave.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+import argparse
+import waybackpy
+
+import code
+import pprint
+
+# Script version
+VERSION = '1.0'
+
+# Options definition
+parser = argparse.ArgumentParser(description="version: " + VERSION)
+parser.add_argument('-i', '--input-file', help='Input file as list of newline-separated FQDN', required = True)
+parser.add_argument('-s', '--output-success', help='Output file to write successfully saved URL')
+parser.add_argument('-f', '--output-failed', help='Output file to write failed attempts to save URL')
+
+def dump_to_file(filename, content):
+    with open(os.path.abspath(filename), mode='w', encoding='utf-8') as fd_output:
+        for element in content:
+            fd_output.write(element + '\n')
+    
+    return None
+
+def waybackpy_save(url, successful_save_attempts, failed_save_attempts):
+    res = True
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+    
+    print("[+] trying to save url:\t\t'%s'" % url)
+    try:
+        save_api = waybackpy.WaybackMachineSaveAPI(url=url, user_agent=user_agent, max_tries=5)
+        save_api.save()
+        
+        if save_api.status_code == 200:
+            print("[+] successfully saved url\t'%s'\n----------" % url)
+            successful_save_attempts.append(url)
+
+    except Exception as e:
+        failed_save_attempts.append(url)
+        res = False
+        print("[!] exception '%s' for url '%s'\n----------" % (e.__class__.__name__, url))
+    
+    finally:
+        return res
+
+def submit(options):
+    successful_save_attempts = []
+    failed_save_attempts = []
+    
+    if os.path.isfile(options.input_file):
+        urls = []
+        with open(options.input_file, mode='r', encoding='utf-8') as fd_input:
+            data = fd_input.read().splitlines()
+            
+            # punydecode
+            data = list(map(lambda fqdn: fqdn.encode('idna').decode(), data))
+            urls = list(map(lambda fqdn: "http://" + fqdn, data)) + list(map(lambda fqdn: "https://" + fqdn, data))
+            
+        if urls:
+            print("[+] %s urls to save\n" % len(urls))
+            
+            for url in urls:
+                waybackpy_save(url, successful_save_attempts, failed_save_attempts)
+            
+            print("[!] number of failed save attempts: %s" % len(failed_save_attempts))
+            pprint.pprint(failed_save_attempts)
+    
+            if options.output_success and successful_save_attempts:
+                dump_to_file(options.output_success, successful_save_attempts)
+            
+            if options.output_failed and failed_save_attempts:
+                dump_to_file(options.output_failed, failed_save_attempts)
+    
+    return
+
+def main():
+    global parser
+    options = parser.parse_args()
+    
+    submit(options)
+    
+    return None
+
+if __name__ == "__main__" :
+    main()
